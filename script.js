9c398f7a42686de78dc4156043c79461f669414f
PututArli <rafaelpututarli@gmail.com>
2026-04-07 15:12:30 +0700

    update sejarah stasi

--- script.js
+++ script.js
@@ -1,3 +1,138 @@
+const stasiData = {
+    'margo-agung': {
+        title: 'Stasi Margo Agung (Pusat)',
+        patron: 'Santo Andreas Rasul',
+        history: 'Gereja ini terletak di desa Marga Agung, Jati Agung. Terbentuk akibat transmigrasi korban letusan Gunung Merapi tahun 1961 dari Magelang, Jawa Tengah. Rombongan pertama tiba 27 Juni 1961. Cikal bakal gereja dimulai dari 9 KK Katolik perdana di Marga Kaya. Gedung gereja pertama (semi permanen) selesai tahun 1962-1963 di Blok C dan diresmikan oleh Bapa Uskup Hermelink Gentiaras SCJ pada 27 Juni 1964.',
+        chairmen: ['Yohakim Noto Siswoyo', 'Yohakim Sosrowasito', 'Petrus Siswosukarto', 'Fx. Edi Suwarno', 'Paulus Sutikno', 'Yohanes Supandi', 'Petrus Sukamto', 'Fx. Heri Hendarto', 'Andreas Kamajaya (2024-Sekarang)']
+    },
+    'marga-lestari': {
+        title: 'Stasi Marga Lestari',
+        patron: 'Santa Maria',
+        history: 'Berbeda dengan Marga Agung, umat di sini bukan dari transmigrasi Merapi, melainkan daerah bukaan tahun 1962. Awalnya bagian dari Gedung Meneng I. Tokoh perintisnya adalah Bapak Ayub dan Bapak Aliun. Gereja pertama dibangun tahun 1968 dengan semangat Persatuan Konten, hasil gotong royong umat Katolik dan warga Muslim setempat. Nama pelindung Santa Maria terinspirasi dari Sendang Sono, ditandai dengan penanaman potongan jari patung Bunda Maria Sendang Sono di bawah altar gereja lama.',
+        chairmen: ['Kirin Siswo Soemarto', 'Ignasius Sis Mardji', 'Robertus Soeryo Martono', 'Yusuf Basuki', 'Gabriel Irawan Edi Soeyono', 'Robertus Yunarto', 'Wiryo Suprayogi (2005)', 'Robertus Yunarto (2008)', 'Antonius Hadi Susanto (2011)', 'Yohanes Seh Hadi (2014)', 'Hariyadi (2017)', 'Yohanes Seh Hadi (2020)', 'Antonius Imam S (2023)', 'Antonius Suhartono (2025)']
+    },
+    'sindang-sari': {
+        title: 'Stasi Sindangsari',
+        patron: 'Santo Stefanus',
+        history: '<strong>Awal Kedatangan Tokoh Perintis (1949\u20131950)</strong><br>Gereja Santo Stefanus telah berdiri sejak tahun 1967. Kehadiran gereja ini tidak lepas dari peran seorang tokoh Katolik dari Jawa, yaitu Bapak Laurensius Sastro Wirono Sukapjo, berasal dari Piton, Kulon Progo, DIY Yogyakarta. Pada tahun 1949, beliau pergi ke Lampung dan menetap di Desa Karang Anyar, Kecamatan Jati Agung. Setahun kemudian, pada tahun 1950, beliau pindah ke Way Rilau (kini Desa Sindangsari, Kecamatan Tanjung Bintang), yang saat ini merupakan kantor perkebunan karet AVD VII milik PT. Nusantara VII. Bapak Sastro dikenal sebagai tokoh yang handal dan banyak berhasil menarik orang untuk masuk ke dalam Gereja Katolik.<br><br><strong>Perintisan Iman dan Pendirian Gereja (1950\u20131967)</strong><br>Pada masa itu, daerah Way Rilau belum memiliki bangunan gereja. Misa dan ibadat diadakan di rumah-rumah umat. Romo yang sering melayani adalah Romo Kam, seorang misionaris dari Belanda. Secara administratif, daerah ini masih masuk wilayah pelayanan Paroki Katedral Kristus Raja, Tanjung Karang. Pada mulanya, perayaan Ekaristi hanya dihadiri 3\u20135 orang. Seiring bertambahnya umat pada tahun 1960-an, pada tahun 1967 didirikanlah gereja atas inisiatif Bapak Sastro Wirono, dibantu Bapak Sunar (alm). Lokasi di Sindangsari (Dusun Umbul Kapuk) tidak berpindah hingga saat ini, dan bangunan aslinya masih berdiri kokoh.<br><br><strong>Perkembangan Umat dan Pelayanan Saat Ini</strong><br>Dari hanya satu Keluarga Katolik pada tahun 1950, kini pada tahun 2025, Stasi Santo Stefanus Sindangsari telah berkembang menjadi 38 Kepala Keluarga atau 87 jiwa. Stasi ini kini masuk ke dalam wilayah pelayanan Paroki Santo Andreas Rasul, Marga Agung.',
+        chairmen: ['Laurensius Sastro Wirono Sukapjo (Perintis, 1950\u20131967)']
+    },
+    'rejomulyo': {
+        title: 'Stasi Rejomulyo',
+        patron: 'Tritunggal Mahakudus',
+        history: '<strong>Asal Usul Desa Rejomulyo (1949\u20131951)</strong><br>Berdirinya Desa Rejomulyo bermula dari pasangan perantauan dari Jawa Tengah, yaitu Bapak Risman (beragama Islam) dan Ibu Jeane Lavre/Rasmani (beragama Katolik). Awalnya tinggal di Perkebunan Rotterdem (kini PRPN VII Unit Trikora). Pada tahun 1949, akibat situasi perang, keluarga ini bersama 135 pekerja lainnya mengungsi ke hutan pedalaman yang kini dikenal sebagai Rejomulyo. Pada tanggal 8 Januari 1951, Desa Rejomulyo diresmikan oleh negara. Nama Rejomulyo berarti "Makmur". Saat ini desa ini terletak di ujung utara Kecamatan Jati Agung, berbatasan dengan Kecamatan Natar.<br><br><strong>Perkembangan Iman Katolik dan Awal Stasi (1966\u20131970)</strong><br>Tahun 1966 menjadi cikal bakal berkembangnya Gereja Katolik di Rejomulyo, diawali dengan satu Keluarga Katolik, yaitu keluarga Bapak Sutrisno. Umat bertumbuh pesat akibat dampak pemberontakan PKI. Umat beribadah di rumah Bapak Kepala Desa, didampingi oleh Pastor Vrankeen. Pelayanan Ekaristi awalnya hanya dua bulan sekali. Stasi ini memiliki tradisi unik: sembayangan ibu-ibu setiap Jumat sore dan sembayangan bersama setiap Rabu malam, keduanya masih lestari hingga saat ini.<br><br><strong>Toleransi dan Pembangunan Gereja (1970)</strong><br>Pada tahun 1970, gereja Katolik didirikan bersamaan dengan pembangunan masjid atas iuran bersama warga\u2014sebuah simbol toleransi yang kuat. Kebersamaan ini juga tampak dalam acara gugur gunung (gotong royong) dan pesta HUT RI.<br><br><strong>Statistik Umat (per November 2024)</strong><br>Jumlah umat: 101 jiwa dari 36 Keluarga Katolik (64 jiwa dewasa, 27 jiwa remaja, 10 jiwa anak-anak).',
+        chairmen: ['Sutrisno (1966\u20131980)', 'Sujiman (1981\u20132000)', 'Dewan Stasi (2001\u2013Sekarang)']
+    },
+    'jatimulyo': {
+        title: 'Stasi Jatimulyo Perdana',
+        patron: 'Santo Fransiskus Xaverius',
+        history: 'Tahun 1968 menandai awal perintisan Gereja Katolik di wilayah Jatimulyo. Awalnya umat menempuh perjalanan jauh ke Way Kandis atau Kedaton. Umat kemudian bergotong royong membeli tanah di Umbul Niti dan membangun Kapel Santo Ignasius berdinding geribik. Pada 2015, renovasi kapel terhenti karena penolakan warga. Tahun 2016, keluarga F.X. Wakidi dan Agnes Siam menginisiasi gereja baru di Perumahan Jatimulyo Perdana. Selesai tahun 2017, gereja ini diresmikan sebagai stasi pada 2019 oleh Mgr. Yohanes Harun Yuwono. Saat ini terdapat 136 jiwa dari 46 KK.',
+        chairmen: ['Nemesius (2019–2022)', 'Andreas Supriono (2022–2023)', 'Harryson Tampubolon (2023–Sekarang)']
+    },
+    'way-galih': {
+        title: 'Stasi Way Galih',
+        patron: 'Santo Yohanes De Britto',
+        history: 'Terletak di Dusun V.A, Desa Way Galih, di tengah perkebunan PTPN VII. Dirintis tahun 1966 oleh Pastor Vranken. Pada 1968, Bapak Ponijo Harjo Sumarto memelopori pembelian tanah, namun terhalang izin kepala desa. Kapel pertama akhirnya dibangun dari bambu dan atap getepe. Tahun 1975, kerangka kapel kayu dari Sindang Sari dipindah ke Way Galih dan dibangun menjadi semi-permanen dengan bantuan Bapak P. Ngatijo. Tahun 1999, Romo Fransiskus Supomo bersama Bapak Petrus Suharyadi merenovasi total kapel ini, yang kemudian diresmikan pada Natal 1999 dengan pelindung Santo Yohanes De Britto. Saat ini umat mencapai 38 KK (117 jiwa).',
+        chairmen: ['Ponijo Harjo Sumarto (1966–?)', 'Ly. Supardi (?–1999)', 'Filipus Marsahid (1999–2001)', 'Paulus Sumardi (2001–2009)', 'Yakobus Maryadi (2009–2015)', 'Andreas Gustami (2015–2018)', 'Fransiskus Xaverius Trimo (2018–Sekarang)']
+    },
+    'sukadamai': {
+        title: 'Stasi Sukadamai',
+        patron: 'Santo Yusuf',
+        history: '<strong>Asal Usul Umat dan Masa Sulit Perintisan (1960\u20131972)</strong><br>Komunitas Umat Katolik di Sukadamai tidak lepas dari berdirinya desa tersebut. Warga, termasuk umat Katolik, adalah pendatang dari Lampung Timur dan Lampung Selatan yang bergotong royong membuka lahan\u2014mayoritas berasal dari Marga Jaya. Pada tahun 1960-an, sengketa lahan dengan pemerintah daerah menyebabkan warga harus berpindah ke Way Abung. Banyak umat Katolik merasa kehilangan arah dan tercerai-berai. Meskipun demikian, enam Keluarga Katolik (KK) bertahan di Sukadamai: keluarga Mbah Pantun, Mbah Parto, Mbah Kamso, Mbah Tembeng, Mbah Lasmo, dan Mbah Sukiman.<br><br><strong>Pembangunan Gereja dan Penolakan Sosial (1972)</strong><br>Pada tahun 1972, Romo Vranken membeli sebidang tanah di RT 006, Dusun II, Desa Sukadamai, dan bersama umat mendirikan gereja permanen berbahan batu bata merah\u2014seluruh biayanya ditanggung Romo Vranken. Di tahun yang sama, umat menghadapi penolakan peribadatan dari masyarakat setempat hingga melibatkan aparat desa. Mereka harus bertahan dan melakukan pendekatan hingga tahun 1983, ketika mediasi dengan tokoh agama dan aparat desa berhasil membuahkan kebebasan beribadah. Pembangunan kedua berlangsung pada tahun 2015\u20132016 melalui gotong royong umat, dengan Romo Hendri dan Romo Roy yang berupaya mendapatkan donatur untuk menyelesaikan pembangunan.<br><br><strong>Dinamika Pelayanan Pastoral dan Perubahan Administrasi (1969\u20132016)</strong><br>Stasi ini pertama kali masuk karya pastoral Paroki St. Yohanes Rasul, Kedaton pada tahun 1969 (Romo Vranken). Karena jarak yang terlalu jauh, Uskup memindahkan karya pastoral: (1) <em>1985</em>\u2014dipindah ke Paroki Hati Kudus Metro, dilayani Romo Jo, Romo Suroyo, dan Romo Suwar; (2) <em>2005</em>\u2014Paroki Marga Agung berdiri; (3) <em>20 April 2016</em>\u2014Stasi St. Yusuf Sukadamai resmi masuk karya pastoral Paroki Marga Agung (Romo Widarman). Sepanjang perpindahan ini, pelayanan Ekaristi hanya satu bulan sekali, menyebabkan umat mengalami kekeringan rohani yang mendalam.<br><br><strong>Perpecahan Internal dan Isu Multikultural</strong><br>Setelah intoleransi terselesaikan (1983), anggota baru dari suku Batak bergabung. Pada tahun 1987, umat bersuku Jawa mendukung anggota Batak menjadi ketua stasi. Namun perselisihan muncul: umat Batak menginginkan Misa dalam tiga bahasa (Indonesia, Jawa, Batak), sementara umat Jawa hanya menginginkan satu bahasa. Akibatnya, umat bersuku Batak terpaksa berpindah dari Gereja St. Yusuf Sukadamai.<br><br><strong>Penamaan Gereja dan Data Umat</strong><br>Pada tahun 1995 disepakati nama pelindung Santo Yusuf\u2014terinspirasi dari kebijaksanaan Santo Yusuf melindungi keluarganya, sebagai cerminan ketahanan umat menghadapi penolakan. Dari enam keluarga sesepuh di tahun 1960-an, hingga Desember 2025, jumlah umat hanya bertambah menjadi <strong>23 Kepala Keluarga</strong>.',
+        chairmen: ['Ketua dari Suku Batak (1987–Tdk Diketahui)', 'Dewan Stasi (Setelah 1987)']
+    },
+    'pendowo': {
+        title: 'Stasi Pendowo',
+        patron: 'Santo Aloysius',
+        history: '<strong>Awal Pembukaan Lahan dan Pembentukan Komunitas Umat (1969–1971)</strong><br>Desa Pancasila mulai dibuka sekitar tahun 1969. Pada masa pembukaan hutan, orang-orang berdatangan tanpa memandang latar belakang agama. Kegiatan keagamaan (ulangan/pengajaran) Katolik mulai dilakukan dengan pendampingan Pak Sutrisno (dari Rejomulyo), Simas (dari Metro), dan Jaiman (dari Natar). Umat Katolik asli desa hanya terdiri dari dua KK: keluarga Petrus Samulyo dan Yusuf Sulam—berasal dari Metro, Bedeng 20 Purwodadi, bukan dari program Transmigrasi. Karena sekitar 30 KK tertarik mengikuti pelajaran agama, pada tahun 1971 terjadi baptisan massal, termasuk orang tua narasumber.<br><br><strong>Dinamika Umat dan Status Awal Stasi (1971–1998)</strong><br>Beberapa tahun setelah baptisan massal, banyak dari 30 KK tersebut berpindah tempat. Sekitar tahun 1978–1979, hanya tersisa 15 KK—dan keturunan merekalah yang bertahan hingga saat ini. Jumlah umat kini stagnan, hanya bertambah dari keturunan. Berdasarkan data Biduk 2024, stasi ini memiliki <strong>13 Kepala Keluarga</strong>. Pada masa awal, komunitas ini belum berstatus stasi, melainkan masih berupa kring/lingkungan (ketua: Yakobus Natal). Sejak 1971, lingkungan ini menginduk langsung ke Katedral Kristus Raja (Romo Van Kamp, Romo Stan, Romo Laton, Romo Dwijo), lalu menjadi bagian dari Paroki St. Yohanes Rasul, Kedaton sekitar 1979–1980. Nama Stasi Santo Aloysius, Pendowo mulai dikenal sekitar tahun 1982.<br><br><strong>Sejarah Pembangunan dan Perpindahan Lokasi Gereja</strong><br>Sejak awal (1971), kapel telah dibangun dengan papan. Tanah gereja merupakan jatah pembagian bukaan lahan karena ketua RT/RW kala itu beragama Katolik. Lokasi gereja sempat berpindah akibat permintaan “tukar guling” dari Pak Sumo (warga non-Katolik), dan lahan gereja kini seluas kurang lebih setengah hektar berkat pembelian tambahan oleh Keuskupan dan yayasan pada masa Romo Hendri. Renovasi pertama (1991): kapel papan direhab dengan batu bata, dipimpin Romo Paul dan Romo Totok dari Paroki Kedaton. Administrasi berpindah ke Paroki Marga Agung sekitar 1998. Renovasi kedua/pembangunan total (2013–2014): kusen keropos mendorong rehabilitasi total. Pada <strong>tahun 2014, Gereja St. Aloysius, Stasi Pendowo diberkati oleh Mgr. Yohanes Harun Yuwono</strong>—momen bersejarah karena sejak awal gereja belum pernah diberkati secara resmi. Pemberkatan dihadiri umat dari hampir seluruh stasi di Paroki Marga Agung.',
+        chairmen: ['Yakobus Natal (Ketua Kring, Awal)', 'Yuventius Prawoto (1982\u2013...)', 'Pardi', 'Yuventius Prawoto (periode ke-2)', 'Martinus Satimin (1990\u2013...)', 'Yohanes Robert Rebin', 'Markus Ponidi', 'Pardi (periode ke-2)', 'Yohanes Sukiran', 'Albertus Yanto (...\u20132025)', 'Markus Ruswantoro (2025\u2013Sekarang)']
+    },
+    'purwotani': {
+        title: 'Stasi Purwotani',
+        patron: 'Santo Petrus',
+        history: '<strong>Awal Pembentukan Komunitas dan Perjuangan Toleransi (1984\u20131990-an)</strong><br>Desa Purwotani terbentuk sekitar tahun 1984. Sebagian besar penduduk, termasuk umat Katolik, adalah pendatang dalam proyek transmigrasi lokal karena bekerja di PT. Midsugoro (Jepang). Umat Katolik perdana berjumpa dan mulai beribadat tahun 1990-an, terdiri dari 8 Keluarga Katolik: Pak Surajan, Pak Albertus Kasimin, Pak Margono, Pak Karinus, Pak Bernardus, Mbah Kasimah, Mbah Petrus Murbadi, dan Mbah Yohanes Saleh. Awalnya merupakan lingkungan di Gereja St. Aloysius Bergen (Paroki Katedral). Pada awal 1990-an, Romo Peco dan katekis Pak Puspayuwono mulai memberikan pelayanan. Umat harus berjuang memperoleh izin dari Kepala Desa dan mendapat pengawasan ibadat karena kecurigaan, tetapi secara perlahan terjalin toleransi yang kuat hingga saat ini tanpa adanya benturan agama.<br><br><strong>Lahan Gereja dan Perpindahan Status Administratif (1992\u20132022)</strong><br>Sekitar tahun 1992, Romo Peco membeli tanah 3/4 hektar untuk gereja lalu ditukar guling oleh Pak Surajan dengan pekarangan 3 rante di tengah kampung. SKT lahan tersebut sekarang disimpan di Paroki Marga Agung atas nama Supriyanto. Menariknya, hingga saat ini umat belum memiliki bangunan gereja permanen; semua ibadat dan Misa diadakan bergiliran di rumah umat\u2014yang juga berfungsi sebagai kunjungan pastoral langsung. Pada 2022, lingkungan tersebut pindah ke Paroki Marga Agung dan baru resmi ditetapkan sebagai <strong>Stasi Santo Petrus Purwotani</strong> oleh Romo Philipus Suroyo.<br><br><strong>Semangat Beribadat dan Data Umat Terkini</strong><br>Sebelumnya Misa hanya diterima pada Minggu ke-5 dan kini umat sering bergabung ke Stasi Marga Lestari di Hari Minggu. Semangat umat sangat tinggi, terbukti dengan partisipasi utuh saat ada ibadah lokal di stasi. Saat ini, jumlah umat Stasi Purwotani tercatat ada <strong>7 Kepala Keluarga (25 jiwa)</strong>.',
+        chairmen: ['Surajan (Ketua Lingkungan, 1990\u2013?)', 'Sempat ada kekosongan ketua', 'Albertus Kasimin (Ketua Lingkungan, ?\u20132021)', 'Benediktus Daman (Ketua Stasi, 2022\u2013Sekarang)']
+    }
+};
+
+function showToast(message, type = 'success') {
+    const toast = document.createElement('div');
+    const bgColor = type === 'error' ? 'bg-red-600' : 'bg-green-600';
+    
+    toast.className = `fixed bottom-6 right-6 px-6 py-4 rounded-xl text-white font-bold shadow-2xl transform transition-all duration-300 translate-y-20 opacity-0 z-50 flex items-center ${bgColor}`;
+    toast.innerHTML = `<span>${message}</span>`;
+    
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.remove('translate-y-20', 'opacity-0');
+    }, 10);
+
+    setTimeout(() => {
+        toast.classList.add('translate-y-20', 'opacity-0');
+        setTimeout(() => toast.remove(), 300);
+    }, 3000);
+}
+
+function openStasiModal(id) {
+    const data = stasiData[id];
+    
+    if (!data) {
+        showToast('Sejarah untuk stasi ini belum tersedia.', 'error');
+        return;
+    }
+
+    const modal = document.getElementById('stasiModal');
+    const content = document.getElementById('modalContent');
+    
+    let chairmenList = data.chairmen.map(name => `<li class="py-1 border-b border-blue-50 text-gray-700">${name}</li>`).join('');
+
+    content.innerHTML = `
+        <h2 class="text-3xl font-bold text-blue-900 mb-2">${data.title}</h2>
+        <p class="text-blue-600 font-bold mb-6 italic">Pelindung: ${data.patron}</p>
+        
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div class="md:col-span-2">
+                <h4 class="font-bold text-blue-800 mb-2 border-l-4 border-blue-800 pl-3">Sejarah Singkat</h4>
+                <p class="text-gray-700 text-justify leading-relaxed">${data.history}</p>
+            </div>
+            <div class="relative">
+                <div class="sticky top-0 bg-white/60 p-5 -mt-5 rounded-2xl backdrop-blur-md border border-white/50 shadow-sm z-10">
+                    <h4 class="font-bold text-blue-800 mb-2 border-l-4 border-blue-800 pl-3">Ketua Stasi</h4>
+                    <ul class="text-sm max-h-64 overflow-y-auto pr-2 custom-scrollbar">
+                        ${chairmenList}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `;
+    
+    modal.classList.remove('hidden');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeStasiModal() {
+    const modal = document.getElementById('stasiModal');
+    modal.classList.add('hidden');
+    document.body.style.overflow = 'auto';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('stasiModal');
+    if (event.target == modal) {
+        closeStasiModal();
+    }
+}
+
+document.querySelectorAll('.btn-sejarah').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const stasiId = this.getAttribute('data-stasi');
+        openStasiModal(stasiId);
+    });
+});
+
 document.body.classList.add('page-transition');
 
 const links = document.querySelectorAll('a[href$=".html"]');
@@ -54,25 +189,6 @@ if (searchInput && stasiContainer) {
             noResultMsg.classList.remove('hidden');
         }
     });
-}
-
-function showToast(message, type = 'success') {
-    const toast = document.createElement('div');
-    const bgColor = type === 'error' ? 'bg-red-600' : 'bg-green-600';
-    
-    toast.className = `fixed bottom-6 right-6 px-6 py-4 rounded-xl text-white font-bold shadow-2xl transform transition-all duration-300 translate-y-20 opacity-0 z-50 flex items-center ${bgColor}`;
-    toast.innerHTML = `<span>${message}</span>`;
-    
-    document.body.appendChild(toast);
-
-    setTimeout(() => {
-        toast.classList.remove('translate-y-20', 'opacity-0');
-    }, 10);
-
-    setTimeout(() => {
-        toast.classList.add('translate-y-20', 'opacity-0');
-        setTimeout(() => toast.remove(), 300);
-    }, 3000);
 }
 
 const contactForm = document.getElementById('contactForm');
